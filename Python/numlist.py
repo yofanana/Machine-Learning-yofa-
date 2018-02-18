b6aaca39e95588178ee879a8324d0a9b15d30705
apple <yofa0905@gmail.com>
2018-02-18 13:04:14 +0900

    ML
    1-1 python List

---
+++ Python/numlist.py
@@ -0,0 +1,22 @@
+
+
+
+
+import random
+import numpy as np
+#1.1
+n=10
+numLits=list(range(1,n+1))
+
+print("list: ",numLits)
+print("{}{}".format("the 3rd: ", numLits[2]))
+print("{}{}".format("the 5th: ", numLits[4]))
+numLits.insert(3,25)
+print("{}{}".format("add 25: ", numLits))
+numLits.pop()
+print("{}{}".format("delete the last item : ", numLits))
+print("{}{}".format("list size : ",len(numLits)))
+print("{}{}".format("list average : ",np.mean(numLits)))
+print("{}{}".format("list max : ",max(numLits)))
+numLits.sort()
+print("{}{}".format("sort list : ",numLits))
